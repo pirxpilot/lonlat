10c778d6cafabbe00e8377dedff303794d1ef738
Damian Krzeminski <pirxpilot@furkot.com>
2024-03-14 22:32:51 +0100

    modernize code

--- test/mongo.js
+++ test/mongo.js
@@ -1,47 +1,54 @@
 const { describe, it } = require('node:test');
-var mongo = require('..').mongo;
+const { mongo } = require('..');
 
-describe('mongo.lonlat', function() {
-  it('should convert string to coordinates array', function() {
+describe('mongo.lonlat', function () {
+  it('should convert string to coordinates array', function () {
     mongo.lonlat('12,13.4').should.eql([12, 13.4]);
     mongo.lonlat('-50.1,-10.4').should.eql([-50.1, -10.4]);
   });
 });
 
-describe('mongo.box', function() {
-  it('should convert string to coordinates array', function() {
-    mongo.box('-50.1,-10.4', '12,13.4').should.eql([[-50.1, -10.4], [12, 13.4]]);
+describe('mongo.box', function () {
+  it('should convert string to coordinates array', function () {
+    mongo.box('-50.1,-10.4', '12,13.4').should.eql([
+      [-50.1, -10.4],
+      [12, 13.4]
+    ]);
   });
-  it('should revert points order when needed', function() {
-    mongo.box('12,13.4', '-50.1,-10.4').should.eql([[-50.1, -10.4], [12, 13.4]]);
+  it('should revert points order when needed', function () {
+    mongo.box('12,13.4', '-50.1,-10.4').should.eql([
+      [-50.1, -10.4],
+      [12, 13.4]
+    ]);
   });
 });
 
-describe('mongo.poly', function() {
-  it('should convert string to coordinates array', function() {
+describe('mongo.poly', function () {
+  it('should convert string to coordinates array', function () {
     mongo.poly('12,13.4,-50.1,-10.4,31,52,11.2,22.3').should.eql([
       [12, 13.4],
       [-50.1, -10.4],
       [31, 52],
-      [11.2,22.3]
+      [11.2, 22.3]
     ]);
     mongo.poly('12,13.4,-50.1,-10.4,31,52,11.2,22.3,0').should.eql([
       [12, 13.4],
       [-50.1, -10.4],
       [31, 52],
-      [11.2,22.3]
+      [11.2, 22.3]
     ]);
   });
 });
 
-describe('mongo.boxes', function() {
-  it('should convert string to array of pairs', function() {
+describe('mongo.boxes', function () {
+  it('should convert string to array of pairs', function () {
     mongo.boxes('12,13.4,-50.1,-10.4,31,52,11.2,22.3').should.eql([
       [
         [-50.1, -10.4],
         [12, 13.4]
-      ],[
-        [11.2,22.3],
+      ],
+      [
+        [11.2, 22.3],
         [31, 52]
       ]
     ]);
@@ -49,8 +56,9 @@ describe('mongo.boxes', function() {
       [
         [-50.1, -10.4],
         [12, 13.4]
-      ],[
-        [11.2,22.3],
+      ],
+      [
+        [11.2, 22.3],
         [31, 52]
       ],
       [
--- test/util.js
+++ test/util.js
@@ -1,21 +1,21 @@
 const { describe, it } = require('node:test');
-var util = require('..').util;
+const { util } = require('..');
 
-describe('parseAllFloats', function() {
-  it('should convert string to array of floats', function() {
+describe('parseAllFloats', function () {
+  it('should convert string to array of floats', function () {
     util.parseAllFloats('12,13.4,-50.1,-10.4,31,52,11.2,22.3').should.eql([
-        12,
-        13.4,
-        -50.1,
-        -10.4,
-        31,
-        52,
-        11.2,
-        22.3
+      12,
+      13.4,
+      -50.1,
+      -10.4,
+      31,
+      52,
+      11.2,
+      22.3
     ]);
   });
 
-  it('should keep array of floats unchanged', function() {
+  it('should keep array of floats unchanged', function () {
     util.parseAllFloats([2, 3, 4]).should.eql([2, 3, 4]);
   });
 
