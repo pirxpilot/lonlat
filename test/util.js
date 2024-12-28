c778ebf9bf044152495eeebfca36c62bd8c4ea21
Damian Krzeminski <pirxpilot@furkot.com>
2024-12-28 12:15:05 -0700

    replace `should` with standard `node:assert`

--- test/util.js
+++ test/util.js
@@ -1,9 +1,10 @@
 const { describe, it } = require('node:test');
+const assert = require('node:assert/strict');
 const { util } = require('..');
 
 describe('parseAllFloats', function () {
   it('should convert string to array of floats', function () {
-    util.parseAllFloats('12,13.4,-50.1,-10.4,31,52,11.2,22.3').should.eql([
+    assert.deepEqual(util.parseAllFloats('12,13.4,-50.1,-10.4,31,52,11.2,22.3'), [
       12,
       13.4,
       -50.1,
@@ -16,7 +17,7 @@ describe('parseAllFloats', function () {
   });
 
   it('should keep array of floats unchanged', function () {
-    util.parseAllFloats([2, 3, 4]).should.eql([2, 3, 4]);
+    assert.deepEqual(util.parseAllFloats([2, 3, 4]), [2, 3, 4]);
   });
 
 });
